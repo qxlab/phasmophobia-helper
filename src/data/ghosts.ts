ace7889b2ca896140bb8e04e2942d736f20cc53f
Aldushin Ivan <ivan.aldush@gmail.com>
2020-10-29 19:56:19 +0300

    feat(Revenant): Add Revenant

--- src/data/ghosts.ts
+++ src/data/ghosts.ts
@@ -1,3 +1,3 @@
 import { Ghost } from '../entities';
 
-export const GHOSTS: Array<Ghost> = ['banshee', 'demon', 'jinn', 'mare', 'oni', 'phantom', 'poltergeist'];
+export const GHOSTS: Array<Ghost> = ['banshee', 'demon', 'jinn', 'mare', 'oni', 'phantom', 'poltergeist', 'revenant'];
--- src/entities/ghost.ts
+++ src/entities/ghost.ts
@@ -2,7 +2,7 @@ import { Lang } from '../hooks';
 import { GHOSTS } from '../data';
 import { getStringByKey } from '../utils';
 
-export type Ghost = 'banshee' | 'demon' | 'jinn' | 'mare' | 'oni' | 'phantom' | 'poltergeist'
+export type Ghost = 'banshee' | 'demon' | 'jinn' | 'mare' | 'oni' | 'phantom' | 'poltergeist' | 'revenant'
 
 export interface GhostTranslationData {
   key: Ghost;
